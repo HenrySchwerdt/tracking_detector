31b9a5dc1ee8c82bb553c462f584dbdad6162e18
heschwerdt <henry.schwerdtner@web.de>
2023-01-15 13:28:28 +0100

    added browse request feature

--- popup/js/script.js
+++ popup/js/script.js
@@ -22,33 +22,40 @@ setInterval(() => {
 
 setInterval(() => {
   chrome.storage.local.get(["requests"], (data) => {
+    
     requests = data.requests;
+    document.getElementById('requests').innerHTML = '';
+    requests.filter(x=>x.blocked).forEach(request => {
+      let innerHtml = `<div>
+        <div class="mui--text-caption mt-1">${request.url}</div>
+        
+        <div class="mui-divider mt-1">
+        </div>
+      </div>`
+      let el = document.createElement('div')
+      el.innerHTML = innerHtml;
+      document.getElementById('requests').appendChild(el);
+    });
+    
     let blocked = data.requests.filter((x) => x.blocked).length;
     document.getElementById("numOfBlockedRequests").innerHTML = blocked;
   });
 }, 200);
 
-document.getElementById('moreInfos').addEventListener("click", () => {
-    console.log("Clicked")
-    buildRequestList();
-})
 
-function buildRequestList() {
-  const container = document.createElement("div");
-  container.id = "requestsholder";
-  requests
-    .filter((x) => x.blocked)
-    .map((x) => {
-       let url=  new URL(
-            x.url
-          );
-      let item = document.createElement("div");
-      let text = document.createTextNode(JSON.stringify(url));
-      item.appendChild(text)
-      return item
-    }).forEach(x => {
-        container.appendChild(x);
-    });
 
-    document.getElementById('requests').appendChild(container);
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
 }
